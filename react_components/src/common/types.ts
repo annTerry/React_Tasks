1d0167093d37e33f5b355ff0b81e7b6157b5ed6c
Anna <89348633+annTerry@users.noreply.github.com>
2023-03-14 23:26:50 +0300

    feat: add lint run and lint fixes

--- react_components/src/common/types.ts
+++ react_components/src/common/types.ts
@@ -1,21 +1,21 @@
 export interface Card {
-    bookName :string,
-    author: string,
-    popularity: number,
-    year: string,
-    translation?: string,
-    cover: string,
-    pages: number,
-    illustration? : string,
-    quantity : number,
-    state? : string
-};
+  bookName: string;
+  author: string;
+  popularity: number;
+  year: string;
+  translation?: string;
+  cover: string;
+  pages: number;
+  illustration?: string;
+  quantity: number;
+  state?: string;
+}
 
 export interface CardProperty {
-    viewName: string,
-    value?: string | number,    
-};
+  viewName: string;
+  value?: string | number;
+}
 
 export interface Popularity {
-    value: number
+  value: number;
 }
