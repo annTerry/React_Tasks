97ad3277737959c71453c018acc2e7b698e2464b
Anna <89348633+annTerry@users.noreply.github.com>
2023-03-15 23:07:36 +0300

    refactor: move static files to static dir

--- react_components/src/common/const.ts
+++ react_components/src/common/const.ts
@@ -1,3 +1,3 @@
 export const NO_COVER = 'none.jpg';
-export const COVERS_URL = 'src/assets/covers/';
-export const DATA_PATH = 'src/assets/data/cards.json';
+export const COVERS_URL = 'static/covers/';
+export const DATA_PATH = 'static/data/cards.json';
--- react_components/vite.config.ts
+++ react_components/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, normalizePath } from 'vite';
+import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: './',
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: normalizePath(path.resolve(__dirname, './static')) + '/[!.]*',
+          dest: './static',
+        },
+      ],
+    }),
+  ],
+});
